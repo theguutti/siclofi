b164af9f37c01190200ebff1b25fa709c85f19b3
íkaro Nogueira Rossotti <129907949+theguutti@users.noreply.github.com>
2025-10-14 14:46:27 -0300

    Add files via upload
    files: cadastroBebe.php, conecta.php, configuracoes.php, entradaChng.php, entradaNew.php, formulaInfantil.php, gerenRelat.php, logout.php, registroObito.php, ressupMen.php, saidaMaternidade.php, saidaPerda.php, saidaReman.php, saidaReturn.php

---
+++ cadastroBebe.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+    <link rel="stylesheet" href="css/styleInicial.css">
+    <link rel="stylesheet" href="css/cadBeberegObito.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    
+    <nav id="sidebar">
+        <div id="sidebar_content">
+            <div id="sidebar_title">
+                <a href="inicial.php"><p class="title">SICLOFI Operacional</p></a>
+            </div>
+            <ul id="side_items">
+                <li class="side-item">
+                    <a href="configuracoes.php">
+                        <i class="fa-solid fa-gear"></i>
+                        <span class="item-description">Configurações</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="cadastroBebe.php">
+                        <i class="fa-solid fa-user-plus"></i>
+                        <span class="item-description">Cadastro de Bebê</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="registroObito.php">
+                        <i class="fa-solid fa-user-xmark"></i>
+                        <span class="item-description">Registro de Óbito</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="ressupMen.php">
+                        <i class="fa-solid fa-chart-simple"></i>
+                        <span class="item-description">Ressuprimento Mensal</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="entradaNew.php">
+                        <i class="fa-solid fa-plus"></i>
+                        <span class="item-description">Entrada - Nova</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="entradaChng.php">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                        <span class="item-description">Entrada - Consultar/Alterar</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="saidaReman.php">
+                        <i class="fa-solid fa-shuffle"></i>
+                        <span class="item-description">Saída - Remanejamento</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="saidaMaternidade.php">
+                        <i class="fa-solid fa-hospital"></i>
+                        <span class="item-description">Saída - Maternidade</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="saidaReturn.php">
+                        <i class="fa-solid fa-rotate-left"></i>
+                        <span class="item-description">Saída - Devolução</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="saidaPerda.php">
+                        <i class="fa-solid fa-xmark"></i>
+                        <span class="item-description">Saída - Perda</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="formulaInfantil.php">
+                        <i class="fa-solid fa-folder"></i>
+                        <span class="item-description">Fórmula Infantil</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="gerenRelat.php">
+                        <i class="fa-solid fa-book"></i>
+                        <span class="item-description">Gerenciador de relatórios</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <form action="logout.php" method="post">
+            <div id="logout">
+                <button type="submit" id="logoutBtn">
+                    <i class="fa-solid fa-right-from-bracket"></i>
+                    <span>Sair</span>
+                </button>
+            </div>
+        </form>
+    </nav>
+
+    <main>
+        <div class="container">
+            
+            <!-- FORM PADRAO -->
+            <div class="form-box active" id="search-form">
+
+                <div class="titulo">
+                    <label id="titulo">Cadastro Bebê</label>
+                </div>
+
+                <div class="conteudo">
+
+                    <div class="field-group">
+                        <label>Nome do Bebê</label>
+                        <input type="text" name="nomeBebeInput-search" autocomplete="off">
+                    </div>
+
+                    <div class="field-group">
+                        <label>CPF do Bebê</label>
+                        <input type="text" name="cpfBebeInput-search" autocomplete="off" minlength="14" maxlength="14" id="cpf-search">
+                    </div>
+                    
+                    <div class="botoes">
+                        <div class="row-buttons">
+                            <button class="btn" id="consultar" onclick="validarConsulta()"> <i class="fa-solid fa-magnifying-glass"></i> Consultar </button>
+                            <button class="btn" id="adicionar" onclick="showForm('new-form')"> <i class="fa-solid fa-plus"></i> Adicionar </button>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            
+            <!-- FORM ADICIONAR -->
+            <div class="form-box" id="new-form">
+
+                <div class="titulo">
+                    <label id="titulo">Cadastro Bebê</label>
+                </div>
+
+                <div class="conteudo"> 
+
+                    <div class="row-items">
+
+                        <div class="field-group">
+                            <label>CPF</label>
+                            <input type="text" name="cpfBebeInput-new" autocomplete="off" minlength="14" maxlength="14" id="cpf-new" required>
+                        </div>
+
+                        <div class="field-group">
+                            <label>CNS - Cartão Nacional de Saúde</label>
+                            <input type="text" name="cnsBebeInput-new" autocomplete="off">
+                        </div>
+
+                        <div class="field-group">
+                            <label>Sinan</label>
+                            <input type="text" name="sinanBebeInput-new" autocomplete="off">
+                        </div>
+                            
+                        <div class="field-group">
+                            <label>Prontuário</label>
+                            <input type="text" name="prontuarioBebeInput-new" autocomplete="off">
+                        </div>
+
+                    </div>
+                    
+                    <div class="row-items">
+                        
+                        <div class="field-group wide">
+                            <label>Nome Completo - Civil</label>
+                            <input type="text" name="nomeBebeInput-new" autocomplete="off" required>
+                        </div>
+
+                        <div class="field-group wide">
+                            <label class="label-control">Nome Completo do Responsável</label>
+                            <input type="text" name="nomeBebeInput-new" autocomplete="off" required>
+                        </div>
+
+                    </div>
+
+                    <div class="botoes">
+                        <div class="row-buttons">
+                            <button class="btn" id="voltar" onclick="showForm('search-form')"> <i class="fa-solid fa-angle-left"></i> Voltar </button>
+                            <button class="btn" id="salvar"> <i class="fa-solid fa-floppy-disk"></i> Salvar </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="modal-overlay" id="messageModal">
+            <div class="modal-box">
+                <h3>Atenção</h3>
+                <p id="modalMessage"></p>
+                <button onclick="closeModal()">OK</button>
+            </div>
+        </div>
+
+    </main>
+
+    <script src="script/cpf.js"></script>
+    <script src="script/forms.js"></script>
+
+</body>
+
+</html>
---
+++ entradaNew.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+    <link rel="stylesheet" href="css/styleInicial.css">
+    <link rel="stylesheet" href="css/entradaNewChng.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    
+    <nav id="sidebar">
+        <div id="sidebar_content">
+            <div id="sidebar_title">
+                <a href="inicial.php"><p class="title">SICLOFI Operacional</p></a>
+            </div>
+            <ul id="side_items">
+                <li class="side-item">
+                    <a href="configuracoes.php">
+                        <i class="fa-solid fa-gear"></i>
+                        <span class="item-description">Configurações</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="cadastroBebe.php">
+                        <i class="fa-solid fa-user-plus"></i>
+                        <span class="item-description">Cadastro de Bebê</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="registroObito.php">
+                        <i class="fa-solid fa-user-xmark"></i>
+                        <span class="item-description">Registro de Óbito</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="ressupMen.php">
+                        <i class="fa-solid fa-chart-simple"></i>
+                        <span class="item-description">Ressuprimento Mensal</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="entradaNew.php">
+                        <i class="fa-solid fa-plus"></i>
+                        <span class="item-description">Entrada - Nova</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="entradaChng.php">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                        <span class="item-description">Entrada - Consultar/Alterar</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="saidaReman.php">
+                        <i class="fa-solid fa-shuffle"></i>
+                        <span class="item-description">Saída - Remanejamento</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="saidaMaternidade.php">
+                        <i class="fa-solid fa-hospital"></i>
+                        <span class="item-description">Saída - Maternidade</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="saidaReturn.php">
+                        <i class="fa-solid fa-rotate-left"></i>
+                        <span class="item-description">Saída - Devolução</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="saidaPerda.php">
+                        <i class="fa-solid fa-xmark"></i>
+                        <span class="item-description">Saída - Perda</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="formulaInfantil.php">
+                        <i class="fa-solid fa-folder"></i>
+                        <span class="item-description">Fórmula Infantil</span>
+                    </a>
+                </li>
+
+                <li class="side-item">
+                    <a href="gerenRelat.php">
+                        <i class="fa-solid fa-book"></i>
+                        <span class="item-description">Gerenciador de relatórios</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <form action="logout.php" method="post">
+            <div id="logout">
+                <button type="submit" id="logoutBtn">
+                    <i class="fa-solid fa-right-from-bracket"></i>
+                    <span>Sair</span>
+                </button>
+            </div>
+        </form>
+    </nav>
+
+    <main>
+        <div class="container">
+            
+            <div class="table-box">
+
+                <div class="titulo">
+                    <label id="titulo">Estoque - Entrada Nova</label>
+                    <label id="UDM">UDM Exemplo</label>
+                </div>
+
+                <div class="conteudo">
+
+                    <div class="row-items">
+                        <div class="field-group">
+                            <label class="label">Tipo de Entrada</label>
+                            <select required>
+                                <option value="Selecione"></option>
+                                <option value="Remanejamento">Remanejamento</option>
+                                <option value="Tratamento ILTB">Tratamento ILTB</option>
+                                <option value="Maternidade">Maternidade</option>
+                                <option value="Remanejamento SICLOM-HIV">Remanejamento SICLOM-HIV</option>
+                            </select>
+                        </div>
+                    </div>
+                    
+                    <button class="btn" id="adicionar" onclick=""> <i class="fa-solid fa-plus"></i> Adicionar </button>
+
+                    <div class="field-group">
+                        
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </main>
+</body>
+</html>
