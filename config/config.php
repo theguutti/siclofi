65aa57154a517de6c6873aa9fc2b800a7ab93398
íkaro Nogueira Rossotti <rossottinog@gmail.com>
2025-10-20 21:10:15 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,1 +1,13 @@
+<?php
+    define('BASE_URL', 'http://localhost/siclofi/');
+    define('SITE_NAME', 'SICLOFI');
 
+    date_default_timezone_set('America/Sao_Paulo');
+
+    header('Content-Type: text/html; charset=utf-8');
+    mb_internal_encoding('UTF-8');
+
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+?>
