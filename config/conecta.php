78186c4b6de438939c5a7d4fdaa3d7cbf2d54fe2
íkaro Nogueira Rossotti <rossottinog@gmail.com>
2025-10-20 21:10:30 -0300

    Update conecta.php

--- config/conecta.php
+++ config/conecta.php
@@ -1,1 +1,21 @@
-
+<?php
+try {
+    $host = "localhost";
+    $dbname = "siclofi";
+    $user = "root";
+    $password = "";
+    
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $user,
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ]
+    );
+} catch(PDOException $e) {
+    die("Erro na conexão: " . $e->getMessage());
+}
+?>
