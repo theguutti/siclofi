c94d760e941c9757ab614d9a2aa0bcd5befb0843
íkaro Nogueira Rossotti <rossottinog@gmail.com>
2025-10-22 17:41:39 -0300

    Update cpf.js

--- scripts/cpf.js
+++ scripts/cpf.js
@@ -1,35 +1,47 @@
-/* TROCAR O FORM QUE ESTÁ VISÍVEL */
-function showForm(formId) {
-    document.querySelectorAll('.form-box').forEach(form => form.classList.remove('active'));
-    document.getElementById(formId).classList.add('active');
-}
+const cpfInputs = document.querySelectorAll('input[id*="cpf"], input[name*="cpf"]');
 
-/* PREENCHER NO MINIMO UM CAMPO */
-function showModal(message) {
-    document.getElementById('modalMessage').textContent = message;
-    document.getElementById('messageModal').classList.add('active');
-}
+cpfInputs.forEach(input => {
+    input.addEventListener('keypress', () => {
+        let inputlength = input.value.length;
+        
+        if (inputlength === 3 || inputlength === 7) {
+            input.value += '.';
+        } else if (inputlength === 11) {
+            input.value += '-';
+        }
+    });
+});
 
-function closeModal() {
-    document.getElementById('messageModal').classList.remove('active');
-}
+// TODO: CALCULAR VALIDEZ CPF
+/*
+CPF EXEMPLO: 111.444.777-__
 
-function validarConsultaCADASTRO() {    
-    const nomeBebe = document.querySelector('input[name="nomeBebeInput-search"]');
-    const cpfBebe = document.querySelector('input[name="cpfBebeInput-search"]');
-    
-    if (!nomeBebe || !cpfBebe) {
-        console.error('Elementos não encontrados!');
-        return false;
-    }
-    
-    const nomeValue = nomeBebe.value.trim();
-    const cpfValue = cpfBebe.value.trim();
-    
-    if (nomeValue === '' && cpfValue === '') {
-        showModal('Preencha pelo menos um dos campos (Nome do Bebê ou CPF do Bebê) para realizar a consulta.');
-        return false;
-    }
-    
-    return true;
-}
+> ETAPA 1 - PRIMEIRO DÍGITO VERIFICADOR (111.444.777-X_).
+    1 - SOMAR O PRODUTO DE CADA DÍGITO ORIGINAL PELA SUA POSIÇÃO.
+    (1*10) + (1*9) + (1*8) + (4*7) + (4*6) + (4*5) + (7*4) + (7*3) + (7*2) = 162
+
+    2 - PEGAR O RESTO DA DIVISÃO ENTRE A SOMA E 11.
+    162 %% 11 = 8
+
+    3 - VERIFICAR SE O RESTO É MAIOR OU MENOR QUE 2.
+        3.1 - MAIOR QUE 2 (SUBTRAIR 11 E O VALOR DO RESTO DA DIVISÃO).
+    11 - 8 = 3
+        3.2 - MENOR QUE 2.
+    DÍGITO VERIFICADOR = 0
+
+> ETAPA 2 - SEGUNDO DÍGITO VERIFICADOR (111.444.777-_X).
+    1 - SOMAR O PRODUTO DE CADA DÍGITO (INCLUINDO O VERIFICADOR) PELA SUA POSIÇÃO.
+    (1*11) + (1*10) + (1*9) + (4*8) + (4*7) + (4*6) + (7*5) + (7*4) + (7*3) + (3*2) = 204
+
+    2 - PEGAR O RESTO DA DIVISÃO ENTRE A SOMA E 11.
+    204 %% 11 = 6
+
+    3 - VERIFICAR SE O RESTO É MAIOR OU MENOR QUE 2.
+        3.1 - MAIOR QUE 2 (SUBTRAIR 11 E O VALOR DO RESTO DA DIVISÃO).
+    11 - 6 = 5
+        3.2 - MENOR QUE 2.
+    DÍGITO VERIFICADOR = 0
+
+> RESULTADO.
+CPF: 111.444.777-35
+*/
