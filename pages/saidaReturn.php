c17ebb982bc2b15d6abca6505303988e599c91de
íkaro Nogueira Rossotti <rossottinog@gmail.com>
2025-11-20 19:53:18 -0300

    Update saidaReturn.php

--- pages/saidaReturn.php
+++ pages/saidaReturn.php
@@ -8,7 +8,8 @@ verificarLogin();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página Inicial</title>
+    <title>SICLOFI</title>
+    <link rel="icon" href="favicon.ico" type="../images/siclofi">
     <link rel="stylesheet" href="../css/styleInicial.css">
     <link rel="stylesheet" href="../css/saidas.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -21,12 +22,6 @@ verificarLogin();
                 <a href="inicial.php"><p class="title">SICLOFI Operacional</p></a>
             </div>
             <ul id="side_items">
-                <li class="side-item">
-                    <a href="configuracoes.php">
-                        <i class="fa-solid fa-gear"></i>
-                        <span class="item-description">Configurações</span>
-                    </a>
-                </li>
 
                 <li class="side-item">
                     <a href="cadastroBebe.php">
@@ -88,13 +83,6 @@ verificarLogin();
                     <a href="saidaPerda.php">
                         <i class="fa-solid fa-xmark"></i>
                         <span class="item-description">Saída - Perda</span>
-                    </a>
-                </li>
-
-                <li class="side-item">
-                    <a href="formulaInfantil.php">
-                        <i class="fa-solid fa-folder"></i>
-                        <span class="item-description">Fórmula Infantil</span>
                     </a>
                 </li>
 
@@ -252,6 +240,7 @@ verificarLogin();
                             <thead>
                                 <tr>
                                     <th>Tipo</th>
+                                    <th>Numeração</th>
                                     <th>Lote</th>
                                     <th>Data de Validade</th>
                                     <th>Data de Entrada</th>
@@ -261,6 +250,7 @@ verificarLogin();
                             <tbody>
                                 <tr>
                                     <td class="tipodata"></td>
+                                    <td class="numdata"></td>
                                     <td class="lotedata"></td>
                                     <td class="datavalidadedata"></td>
                                     <td class="dataentradadata"></td>
